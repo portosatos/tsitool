7223cf2806af835d6bb1495e84e955bb0353d10b
portosatos <emirdastanov8@gmail.com>
2023-05-08 14:54:19 +0600

    header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <header>
             {/* <a href="https://tsiauca.kg/ru/"><img src="../media/tsi-logo.png" alt=""></a> */}
             <ul>
-                <li><a style="text-decoration: underline; text-decoration-color: #7985CE ;" href="../pages/">Main</a></li>
+                {/* <li><a style="text-decoration: underline; text-decoration-color: #7985CE ;" href="../pages/">Main</a></li> */}
                 <li><a href="../pages/">News</a></li>
                 <li><a href="../pages/">Telegram</a></li>
                 <li><a href="../pages/">About</a></li>
