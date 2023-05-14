d4da537226ad0b678fbed0dc07308f116d202d11
portosatos <emirdastanov8@gmail.com>
2023-05-14 20:09:27 +0600

    commit

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,31 +1,31 @@
 // Header.js
 import "./header.css";
-import { Link, useLocation } from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import tsi__logo from "../../media/tsi-logo.png";
 
 const Header = () => {
-  const location = useLocation();
-  const isActive = (path) => (location.pathname === path ? "active" : "");
+    const location = useLocation();
+    const isActive = (path) => (location.pathname === path ? "active" : "");
 
-  return (
-    <header>
-      <Link to="https://tsiauca.kg/ru/">
-        <img src={tsi__logo} alt="" />
-      </Link>
-      <ul>
-        <li>
-          <Link to="/main" className={isActive("/main")}>
-            Main
-          </Link>
-        </li>
-        <li>
-          <Link to="/telegram" className={isActive("/telegram")}>
-            Telegram
-          </Link>
-        </li>
-      </ul>
-    </header>
-  );
+    return (
+        <header>
+            <Link to="/">
+                <img src={tsi__logo} alt=""/>
+            </Link>
+            <ul>
+                <li>
+                    <Link to="/main" className={isActive("/main")}>
+                        Main
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/telegram" className={isActive("/telegram")}>
+                        Telegram
+                    </Link>
+                </li>
+            </ul>
+        </header>
+    );
 };
 
 export default Header;
