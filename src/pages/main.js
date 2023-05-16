6ed80ece873f451987a4c2f82a7be9a68cffb85e
portosatos <emirdastanov8@gmail.com>
2023-05-16 10:08:26 +0600

    commit

--- src/pages/main.js
+++ src/pages/main.js
@@ -19,7 +19,7 @@ function Main() {
             <p className="section2-text">1. You will be able to see your Minor watch. To do this, you will need to log in through your AuCash account, as the Minor is confidential information.</p>
             <p className="section2-text">2. Calculation of your GPA. For this you will need to enter your current scores, and then our bot will calculate your approximate GPA.</p>
             <p className="section2-text"> 3. Receiving a task from Azem. We've decided to improve the system for receiving Assignments from Azem. This bot will send you notifications when Azem needs help, so you'll always know what's going on.</p>
-            <p className="section2-text">4. More to come, more features will be added later</p>
+            <p className="section2-text">4. More to come, more features will be added later.</p>
             <p className="section2-text"></p>
             <p className="section2-text"></p>
         </section>
